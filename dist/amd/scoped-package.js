2b02eb14203ca15a9088765cff98280dea4c81a0
Kevin Phillips <kevin@bitovi.com>
2017-10-03 16:37:33 -0500

    1.0.0

---
+++ dist/amd/scoped-package.js
@@ -0,0 +1,8 @@
+/*@phillipskevin/scoped-package@0.0.0#scoped-package*/
+define(['exports'], function (exports) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.default = function () {
+        return 'This is the @phillipskevin/scoped-package plugin';
+    };
+});
---
+++ dist/cjs/scoped-package.js
@@ -0,0 +1,6 @@
+/*@phillipskevin/scoped-package@0.0.0#scoped-package*/
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.default = function () {
+    return 'This is the @phillipskevin/scoped-package plugin';
+};
